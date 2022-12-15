767a758511b5de65c23eeec804be0b32cbc70570
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-12-15 22:57:10 +0100

    feat: exclude excluded files from navigation and filetree too

--- mkdocs_exclude_tagged_files/plugin.py
+++ mkdocs_exclude_tagged_files/plugin.py
@@ -1,9 +1,12 @@
-from typing import List, Optional
+import typing
+from typing import Optional
 
+import frontmatter
 import mkdocs.config
 import mkdocs.config.config_options
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import Files
 from mkdocs.structure.pages import Page
 
 
@@ -13,11 +16,29 @@ class ExcludeTaggedFilesPluginConfig(mkdocs.config.base.Config):
 
 
 class ExcludeTaggedFilesPlugin(BasePlugin[ExcludeTaggedFilesPluginConfig]):
-    def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> Optional[str]:
-        if "tags" in page.meta:
-            for tag in page.meta["tags"]:
-                if self.config.strip_leading_hashtags and str(tag).startswith("#"):
+    def is_page_excluded(self, meta: typing.Dict):
+        if 'tags' in meta:
+            for tag in meta['tags']:
+                if self.config.strip_leading_hashtags and str(tag).startswith('#'):
                     tag = str(tag)[1:]
 
                 if tag in self.config.tags_to_exclude:
-                    return ""
+                    return True
+
+            return False
+
+    def on_files(self, files: Files, *, config: MkDocsConfig) -> Optional[Files]:
+        for file in files.documentation_pages():
+            with open('./docs/' + file.src_uri, 'r') as raw_file:
+                metadata = frontmatter.load(raw_file).metadata
+
+                if self.is_page_excluded(metadata):
+                    files.remove(file)
+
+        return files
+
+    def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> Optional[str]:
+        if self.is_page_excluded(page.meta):
+            return ''
+
+        return output
