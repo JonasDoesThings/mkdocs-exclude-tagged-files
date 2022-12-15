e73e1af478ac04274be63a060033b271bf31c960
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-12-15 23:00:28 +0100

    build: add python-frontmatter as dependency to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=(this_directory / "README.md").read_text(),
     long_description_content_type='text/markdown',
     keywords=['mkdocs', 'mkdocs-plugin'],
-    install_requires=['mkdocs'],
+    install_requires=['mkdocs', 'python-frontmatter'],
 
     entry_points={
         'mkdocs.plugins': [
